db367ea7c08ed91e73ad11fb263c26b5a9cc609d
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-29 00:48:37 -0500

    Statistics in R

---
+++ statistics_in_r.R
@@ -0,0 +1,21 @@
+# Statistics in r
+
+# What is statistics?
+# 1. The field of statistics - the practice and study of collecting and analysing data.
+# 2. A summary statistics - a fact about or suammary of some data
+#_________________________________________________________________________________________
+# Type of Statistics:-
+#1. Descriptive statistics: Describe and summarize data
+# For eg.:- 
+#2. Inferential statistics: Use a sample of data to make inferences about a large population
+# For eg.:-
+#_________________________________________________________________________________________
+# Types of Data:-
+
+# Numerice (Quantitative):-
+# 1. Discrete (Measured)
+# 2. Continuous (Counted)
+
+# Categorical (Quantitative):-
+# 1. Nominal (Unordered)
+# 2. Ordinal (Ordered)
