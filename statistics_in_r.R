4e9745cd4d882ec327d49195bf541f9574ebef39
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-29 01:27:01 -0500

    mean median mode basic

--- statistics_in_r.R
+++ statistics_in_r.R
@@ -19,3 +19,51 @@
 # Categorical (Quantitative):-
 # 1. Nominal (Unordered)
 # 2. Ordinal (Ordered)
+
+#__________________________________________________________________________________________
+# Measure of center:-
+
+# 1. Mean: 
+#          Mean = (number of obs.) / (total obs.)
+# In mean is calculated by mean()
+
+# 2. Median: 
+# The median of a data set is the middle number of the ordered values. 
+# We can find the median by ordering the values and finding the middle number.
+# By dividing 50 % from both side.
+# For Eg:- 
+# 4,5,9,8,7,8,1  => so here 8 is the median
+# 4,5,9,8,7,7,8,1  => so here (8+7)/2=> 7.5 is the median
+# In R median() function is used.
+
+# 3. Mode:
+# The mode is the number that occurs most often in a data set.
+# For Eg:-
+# 4,5,9,8,7,7,8,1
+# First, order the numbers from least to greatest.
+# 1,4,5,7,7,8,8,9
+# Then, find the number that occurs the most.
+# 7 is the mode.
+#  It is used in Categorical
+
+#_______________________________________________________________________________
+# For this lab, and many others in the future, we will use the following R packages:
+#   
+# dplyr: for data wrangling
+# ggplot2: for data visualization
+# infer: for tidy inference
+#_______________________________________________________________________________
+# Which Measure of center to use (mean, median or mode) and When?
+
+msleep   #Introduction to dplyr: mammal sleep dataset called msleep
+
+# In Skew, median is better to use
+
+
+
+
+
+
+
+
+
